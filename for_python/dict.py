e5849903a6ef46471b24a088a1bf545ea0a60f23
Raunak jain <rony.jain108@gmail.com>
2018-06-12 22:34:03 -0500

    dictionary

---
+++ for_python/dict.py
@@ -0,0 +1,11 @@
+
+zipcode = { 'aaa': 33459, 'bbb': 54529, 'ccc': 44330 }
+print (zipcode)
+print(zipcode['aaa'])
+print(zipcode.keys())
+print(list(zipcode.keys()))
+print(zipcode.values())
+print(list(zipcode.values()))
+weights = dict(kevin=160, bob=240, kayla=135)
+print(weights)
+
