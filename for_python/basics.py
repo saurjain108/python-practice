3ee150978f461be5b6175b5ea3779596c1cde8d2
Raunak jain <rony.jain108@gmail.com>
2018-06-12 19:01:21 -0500

    basics information

--- for_python/basics.py
+++ for_python/basics.py
@@ -1,7 +1,8 @@
+
 #! /usr/bin/env python3.6
 print ("hii")
 print ("haha"*4)
 print ("Pass" + "word")
 print ("FJBBFJBFJvfnv".lower())
-print ("kkfkvkfmmvkf".upper())
-
+print ("ALL ARE IN UPPER CASE".upper())
+ 
