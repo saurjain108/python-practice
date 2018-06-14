b0237ab396776926426f60cc133d0a5fca59d5f4
Raunak jain <rony.jain108@gmail.com>
2018-06-13 23:55:03 -0500

    adding logical ops and for loop example

---
+++ for_python/for_1.py
@@ -0,0 +1,4 @@
+list_of_points = [(1,2), (3,7), (7,8)]
+for x,y in list_of_points:
+	print (f"x: {x}, y: {y}")
+
---
+++ for_python/logical_ops.py
@@ -0,0 +1,16 @@
+name = ""
+print(not name)
+if not name:
+	print(" the person is unknown")
+first = ""
+last = "qwerty"
+
+if (first or last):
+	print(" either first or last is available")
+
+if (first and last):
+	print ("This will not output anything")
+first_name = (first or "keypad")
+print(first_name)
+
+
