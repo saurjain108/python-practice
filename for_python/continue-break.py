990607bc0f82d1aa753c462f0389cfa55d32df36
Raunak jain <rony.jain108@gmail.com>
2018-06-13 20:59:55 -0500

    adding loops

---
+++ for_python/continue-break.py
@@ -0,0 +1,9 @@
+x = 0
+while x <=10:
+	if x%2 ==0:
+		print( str(x) + " is a even number")
+		break
+	else:
+		print ( str(x) + " is odd number")
+		x+=1
+
---
+++ for_python/for-while.py
@@ -0,0 +1,11 @@
+
+x = 0
+while x <=10:
+	if x%2 == 0:
+		print( str(x) + " This is a even number")
+		
+	else:
+		print( str(x) + " This is odd number")
+	x+=1
+
+
