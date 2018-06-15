9cc0aee4c95c434fb1652beb2a93fbd58c2740fd
Raunak jain <rony.jain108@gmail.com>
2018-06-14 20:51:10 -0500

    adding another file

---
+++ for_python/agee.py
@@ -0,0 +1,5 @@
+name = input("what is your name? ")
+birthday = input("what is your birthdate? ")
+age=int(input("what is your age? "))
+print(f"{name} was born on {birthday} and he is {age} years old")
+
