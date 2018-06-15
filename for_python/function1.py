96d6af12d02f9b0e0bb78a2117d9627bfb55ef54
Raunak jain <rony.jain108@gmail.com>
2018-06-14 21:57:47 -0500

    first function

---
+++ for_python/function1.py
@@ -0,0 +1,17 @@
+def name():
+	a = input("what is your name? ")
+	print(a)
+name()	
+
+def age(x):
+#	x = (input("what is your age? "))
+	print(f" And he is {x} years old")
+age(23)
+
+
+def add_two(c):
+	return c+5
+result = add_two(6)
+print(result)
+
+
