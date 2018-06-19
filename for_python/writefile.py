7976c1422398d2a3024fad9ddbedfcd7b38dfd6d
Raunak jain <rony.jain108@gmail.com>
2018-06-18 21:22:46 -0500

    adding a code for reading a file and writting data in newly craeted file

---
+++ for_python/writefile.py
@@ -0,0 +1,22 @@
+#this is use for writting the conent of one file into anotheer file
+
+a = open('abc.txt','r')
+# opened the file which we already have available in the sysyem
+
+b = open('new_abc.txt', 'w')
+#created a file in which we are going to write the data
+
+b.write(a.read())
+#read the data from a and write that in b
+
+b.close()
+#close the file
+
+b= open(b.name, 'r+')
+#open the file with "r+", which will allow to read and write in the file 
+
+print(b.read())
+#printing the content of the new file
+
+
+
