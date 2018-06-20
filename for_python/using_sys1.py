f3e0fbf35bbc72016eca19cc284eb96aee67cc68
Raunak jain <rony.jain108@gmail.com>
2018-06-19 23:03:47 -0500

    adding read-write file and sys.argv file

---
+++ for_python/using_sys1.py
@@ -0,0 +1,6 @@
+import sys
+print(f"*{sys.argv[0]}* is the name of script") 
+print(f"positional arguments: {sys.argv[1:]}")
+print(f"The first argument: {sys.argv[1]}")
+print(f"The lenght of the argements is {len(sys.argv)}")
+
