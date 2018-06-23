f49b62813280640e7d2d0cb7cb3f091f01c934f3
Raunak jain <rony.jain108@gmail.com>
2018-06-23 01:02:49 -0500

    modifying the file from previous file

--- for_python/arg_parse.py
+++ for_python/arg_parse.py
@@ -5,7 +5,14 @@ parser = argparse.ArgumentParser(description = 'read the file in reverse order')
 parser.add_argument('filename', help ='the file to read')
 parser.add_argument('--limit', '-l',type = int , help = 'the number of lines to be read')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+
 args = parser.parse_args()
-print(args)
-
-
+with open(args.filename) as f:
+	lines = f.readlines()
+	lines.reverse()
+	
+	if args.limit:
+		lines = lines[:args.limit]
+	for line in lines:
+		print(line.strip()[::-1])
+	
