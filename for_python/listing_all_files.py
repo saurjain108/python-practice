c8d00c953c0eea074ce922e861b27381001c265a
Raunak jain <rony.jain108@gmail.com>
2018-06-30 00:30:05 -0500

    iterate through all files and finding the normal files and directories

--- for_python/listing_all_files.py
+++ for_python/listing_all_files.py
@@ -3,6 +3,11 @@ import os
 way = r"/home/raunak/for_python"
 
 for i in (os.listdir(way)):
-	
 	print(i)
-
+for roots, dirs, files in os.walk(way):
+	for file in files:
+		print("file= %s" %file)
+	for dir in dirs:
+                print("directory =%s" %dir)	
+#	for root in roots:
+#               print(root)
