47abd35972b8a419b868ea0a76b33645ee8fccb8
Raunak jain <rony.jain108@gmail.com>
2018-07-09 00:49:33 -0500

    adding basic information in first file

--- for_python/for_regix/first_regex.py
+++ for_python/for_regix/first_regex.py
@@ -5,3 +5,15 @@ sequence = "Python"
 if re.match(pattern, sequence):
   print("Match is success!")
 else: print("match is not success !")
+
+# the '.' is used for any character except new line
+a = re.search(r'Pyt..n', 'Python').group()
+print(a)
+
+#the '\w' is used for any single letter or digit or underscore	
+b= re.search(r'P\wtho\w', 'Python').group()
+print(b)
+
+# the '\W' matches with the any character except the \w group
+c = re.search(r'R\Wunak', 'R@unak').group()
+print (c)
