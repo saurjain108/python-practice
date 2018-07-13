307cb887b65559e8c6f5988dd8835b3888ec43e1
saurjain108 <rony.jain108@gmail.com>
2018-07-12 22:31:05 -0500

    Update searching_name.py
    Adding a code which will avoid the word gov

--- for_python/for_regix/searching_name.py
+++ for_python/for_regix/searching_name.py
@@ -7,7 +7,8 @@ name.close()
 
 """print(re.search(r'[r]aunak',data))
 print(re.search(r'[j]ain',data))
-print(re.findall(r'\(?\d{3}\)?-?\s?\d{3}-\d{4}',data))
+print(re.findall(r'\(?\d{3}\)?-?\s?\d{3}-\d{4}',data)) # finding all contact numbers
 print(re.findall(r'\w*, \w+', data))"""
-print(re.findall(r'[\w\d+.-]+@[\d\w.]+',data))
-print(re.findall(r'\b[trehous]{9}\b',data, re.IGNORECASE))
+print(re.findall(r'[\w\d+.-]+@[\d\w.]+',data)) 
+print(re.findall(r'\b[trehous]{9}\b',data, re.IGNORECASE)) # finds all treehouse words
+print(re.findall(r'\b@[-\b\d.]*[^gov\t]+\b',data,re.IGNORECASE)) # ignores the gov 
