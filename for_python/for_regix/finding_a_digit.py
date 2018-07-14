a02d885f143ccb6e01b6214cb3c67bfbab7e08b1
Raunak jain <rony.jain108@gmail.com>
2018-07-14 10:24:46 -0500

    Adding a finding a digit and a full regex code which can find the information of all, group them and print them

---
+++ for_python/for_regix/finding_a_digit.py
@@ -0,0 +1,4 @@
+import re
+string = "1234567890"
+print(re.findall(r'[^567]', string))
+
--- for_python/for_regix/searching_name.py
+++ for_python/for_regix/searching_name.py
@@ -1,5 +1,4 @@
 import re
-
 name = open("names.txt", 'r')
 data = name.read()
 name.close()
@@ -9,7 +8,13 @@ name.close()
 print(re.search(r'[j]ain',data))
 print(re.findall(r'\(?\d{3}\)?-?\s?\d{3}-\d{4}',data)) # finding all contact numbers
 print(re.findall(r'\w*, \w+', data))"""
-print(re.findall(r'[\w\d+.-]+@[\d\w.]+',data)) 
-print(re.findall(r'\b[trehous]{9}\b',data, re.IGNORECASE)) # finds all treehouse words
-print(re.findall(r'\b@[-\b\w.]*[^gov\t]+\b',data,re.IGNORECASE)) # ignores the gov 
-print(re.findall(r'\b[-\w]+,\s[-\w ]+[^\t\n]',b,re.X))# searching all names
+#print(re.findall(r'[\w\d+.-]+@[\d\w.]+',data)) 
+#print(re.findall(r'\b[trehous]{9}\b',data, re.IGNORECASE)) # finds all treehouse words
+#print(re.findall(r'\b@[-\b\w.]*[^gov\t]+\b',data,re.IGNORECASE)) # ignores the gov 
+#print(re.findall(r'\b[-\w]+,\s[-\w ]+[^\t\n]',data,re.X))# searching all names
+line = re.compile(r'^(?P<name>[-\w ]*,\s[-\w ]+)\t(?P<email>[-\w\d.+]+@[-\w\d.]+)\t(?P<contact>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t(?P<job>[\w\s.]+,\s[\w\s.]+)\t?(?P<twitter>@[\w\d]+)?$',re.X|re.MULTILINE)
+#print(line)
+print(re.search(line, data).groupdict())
+print(line.search(data).groupdict())
+for match in line.finditer(data):
+	print(match.group('name','email','contact'))
