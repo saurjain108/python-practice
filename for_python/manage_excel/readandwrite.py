d83af2eb3ac663c42d0ce3822fa50c4153e7438f
Raunak Jain <rony.jain108@gmail.com>
2019-09-01 13:03:20 -0500

    Update readandwrite.py

--- for_python/manage_excel/readandwrite.py
+++ for_python/manage_excel/readandwrite.py
@@ -1,1 +1,9 @@
 
+import csv
+with open('SampleCSVFile_119kb.csv', 'r') as filee:
+    aaa = csv.reader(filee)
+    with open('newest.csv', 'w') as bbb:
+        csv_writer = csv.writer(bbb)
+        for i in aaa:
+            csv_writer.writerow(i)
+            
